9ae190810150b8de933e73e0d65213f20d811d0a
Z Norman <65469304+zacharyanorman@users.noreply.github.com>
2025-06-24 13:29:41 -0400

    Add files via upload

---
+++ genres.py
@@ -0,0 +1,7 @@
+genres_list = {
+    '5763': 'Drama',
+    '34399': 'Comedy',
+    '43040': 'Action',
+    '8711': 'Horror',
+    '1492': 'Sci-Fi & Fantasy'
+}
---
+++ secrets.py
@@ -0,0 +1,4 @@
+# secrets.py (DO NOT share or upload this file)
+
+netflix_api_key = "your_unogs_api_key_here"
+omdb_api_key = "your_omdb_api_key_here"
